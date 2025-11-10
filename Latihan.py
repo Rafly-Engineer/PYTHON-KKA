5a0885dfcc81f5c95cd0765537e862f835295321
Rafly-Engineer <muhammad_rachman_33rpl@student.smktelkom-mlg.sch.id>
2025-11-10 11:17:24 +0700

    Add files via upload

---
+++ Latihan.py
@@ -0,0 +1,14 @@
+a=3
+print(type(a))
+print("hallo python")
+#data Float
+b=4.5
+print(type(b))
+#data Str
+nama="Sinta"
+print(type(nama))
+#data list
+angka=[1,2,3,4,5]
+print(type(angka))
+angka.append(6)
+print(angka)
---
+++ analisis_data.py
@@ -0,0 +1,29 @@
+import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
+
+data = pd.read_csv('nilai_siswa.csv')
+print[data.head()]
+
+data.info()
+data.head()
+data.describe()
+
+print("Rata-rata:", data.gr['Nilai'].mean())
+print("Median:", data['Nilai'].median())
+print("Modus:", data['Nilai'].mode()[0])
+
+matematika = data[data['Mapel'] == 'Matematika']
+print(matematika)
+
+data.groupby('Mapel')['Nilai'].agg(['max','min'])
+
+rata = data.groupby('Mapel')['Nilai'].mean()      
+plt.title('Rata-Rata Nilai per Mapel')
+plt.xlabel('Mata Pelajaran')
+plt.ylabel('Nilai Rata-Rata')
+plt.show()
+
+sns.boxplot(x='Mapel', y='Nilai', data=data)
+plt.title('Sebaran Nilai per Mata Pelajaran')
+plt.show()
